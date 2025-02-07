52a5466a312cd94c3b716cbd16e9ed3cf76f391c
DevNathanC <nathancaetano599@gmail.com>
2025-02-07 19:21:05 -0300

    resolvendo os erros

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -4,21 +4,21 @@ const screen = {
         this.userProfile.innerHTML = `<div class="info">
                          <img src="${user.avatarUrl}" alt="Foto do perfil do usuário" />
                          <div class="data">
-                             <h1>${user.name} ?? 'Não possui nome cadastrado'</h1>
-                             <p> ${user.bio} ?? Não possui bio cadastrada</p>
+                             <h1>${user.name ?? 'Não possui nome cadastrado'}</h1>
+                             <p> ${user.bio ?? 'Não possui bio cadastrada'}</p>
                          </div>
                          </div>`
         
-        let repositoriesitens = ''
-        user.repositories.forEach(repo => repositoriesitens += `<li><a href= "${repo.html_url}" target= "_blank>${repo.name}></a></li>`)
-
+        let repositoriesItens = ''
+        
+        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
+        
         if(user.repositories.length > 0){
-            this.userProfile.innerHTML += `<div class = "repositories section">
+            this.userProfile.innerHTML += `<div class="repositories section">
                                                 <h2>Repositórios</h2>
-                                                <ul>${repositoriesitens}</ul>
+                                                <ul>${repositoriesItens}</ul>
                                             </div>`
         }
-
     },
     renderNotFound(){
         this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,7 +1,10 @@
 import { baseUrl, repositoriesQuantity } from "../variables.js"
 
 async function getRepositories(userName){
-    const response = await fetch(`${baseUrl}/${userName}/repos?${repositoriesQuantity}`)
+    const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
+    if (!response.ok) {
+        throw new Error('Erro ao buscar repositórios');
+    }
     return await response.json()
 }
 
